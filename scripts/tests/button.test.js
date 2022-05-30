fd4bc4b9f74f0bab5b44c8e03a9e8e72bade2b6f
adamhatton <adamdhatton@gmail.com>
2022-05-30 09:48:02 +0000

    Add html and javascript tests from code-along video

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,19 @@
+const { TestScheduler } = require("jest");
+const buttonClick = require("../button");
+
+beforeEach( () => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.write(fileContents);
+    document.close();
+});
+
+describe("DOM test", () => {
+    test("expects p content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    });
+});
